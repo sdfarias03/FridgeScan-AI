2007658fd220c57362c6842a5b49bbec6c83710a
estrellaangel <ella34138@gmail.com>
2025-01-13 18:01:42 -0700

    Updating application to scan fridge.

---
+++ Pods/Target Support Files/Pods-TestingForFridgeScan-TestingForFridgeScanUITests/Pods-TestingForFridgeScan-TestingForFridgeScanUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestingForFridgeScan_TestingForFridgeScanUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TestingForFridgeScan_TestingForFridgeScanUITests
+@end
